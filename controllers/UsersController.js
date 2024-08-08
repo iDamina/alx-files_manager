9592282a6a9fcb5a1cbaa2eff811c625f7a6d9a4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-08 02:52:18 +0100

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,30 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.client.db().collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = await usersCollection.insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: newUser.insertedId, email });
+  }
+}
+
+export default UsersController;
